b417ea5e888d5d0593bb40035dbeb48eff85e22b
Dmitriy-boytsev <141399911+Dmitriy-boytsev@users.noreply.github.com>
2024-07-21 22:03:50 +0300

    Add templates and errors

--- crud.py
+++ crud.py
@@ -1,46 +1,70 @@
-from fastapi import Body, FastAPI,status
-
-from db import messages_db
+from fastapi import FastAPI, status, Body, HTTPException, Request, Form
+from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from typing import List
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+messages_db = []
+templates = Jinja2Templates(directory="templates")
+
+
+class Message(BaseModel):
+    id: int = None
+    text: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples":
+                [
+                    {
+                        "text": "Simple message",
+                    }
+                ]
+        }
+    }
+
 
 @app.get("/")
-async def get_all_messages() -> dict:
-    """Функция для вывода всех записей из словаря message_db"""
-    return messages_db
+async def get_all_messages(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse("message.html", {"request": request, "messages": messages_db})
 
 
-@app.get("/message/{message_id}")
-async def get_message(message_id: str) -> str:
-    """Функция для вывода записи из словаря message_db с принятым message_id"""
-    return messages_db[message_id]
+@app.get(path="/message/{message_id}")
+async def get_message(request: Request, message_id: int) -> HTMLResponse:
+    try:
+        return templates.TemplateResponse("message.html", {"request": request, "message": messages_db[message_id]})
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Message not found")
 
 
-@app.post("/message", status_code=status.HTTP_201_CREATED)
-async def create_message(message: str = Body()) -> str:
-    """Функция для добавляения новых записей при запросе на /message"""
-    current_index = len(messages_db)
-    messages_db[current_index] = message #Длинна словаря на 1 будет больше, чем индексы внутри нее и таким образом мы добавляем новый индекс в словарь
-    return f'Message created'
+@app.post("/", status_code=status.HTTP_201_CREATED)
+async def create_message(request: Request, message: str = Form()) -> HTMLResponse:
+    messages_db.append(Message(id=len(messages_db), text=message))
+    return templates.TemplateResponse("message.html", {"request": request, "messages": messages_db})
 
 
 @app.put("/message/{message_id}")
-async def update_message(message_id: str, message: str = Body()) -> str:
-    """Изменение записи, получая message_id и message из параметра пути и запроса"""
-    messages_db[message_id] = message
-    return f'Message update!'
+async def update_message(message_id: int, message: str = Body()) -> str:
+    try:
+        edit_message = messages_db[message_id]
+        edit_message.text = message
+        return f"Message updated!"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Message not found")
 
 
 @app.delete("/message/{message_id}")
-async def delete_message(message_id: str) -> str:
-    """Удаляет записи из словаря message_db с принятым message_id"""
-    messages_db.pop(message_id)
-    return f'Message {message_id } delete!'
+async def delete_message(message_id: int) -> str:
+    try:
+        messages_db.pop(message_id)
+        return f"Message ID={message_id} deleted!"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Message not found")
 
 
 @app.delete("/")
 async def kill_message_all() -> str:
-    """При запросе к этой функции полностью удалит словарь message_db"""
     messages_db.clear()
-    return f'All message delete! Your bd clear!'
+    return "All messages deleted!"
--- db.py
+++
@@ -1,6 +0,0 @@
-messages_db = {
-    "0": "First post in FastAPI",
-    "1": "Two",
-    "2": "Three",
-    "3": "Fore"
-}
